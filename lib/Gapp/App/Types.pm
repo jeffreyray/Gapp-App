56b785197d97e7a5a754aa902b72a9f70c257dbc
Jeffrey Ray <jeffrey.hallock@gmail.com>
2012-10-08 03:39:32 -0400

    hooks

---
+++ lib/Gapp/App/Types.pm
@@ -0,0 +1,20 @@
+package Gapp::App::Types;
+
+use MooseX::Types -declare => [qw(
+GappAppHookAction
+)];
+
+use MooseX::Types::Moose qw( Str );
+
+subtype GappAppHookAction
+    => as Str
+    => where { $_ eq 'aggregate' || $_ eq 'halt' }
+    => message { "hook action must be 'aggregate' or 'halt'" };
+
+# accept aggr* for 'aggregate'
+coerce GappAppHookAction
+    => from Str
+    => via { /^aggr/ ? 'aggregate' : undef };
+
+
+1;
